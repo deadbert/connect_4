30eea3b7ba5361ce4161b058d43a6d866b738e29
Nick Spencer <strangebrew202@gmail.com>
2023-07-21 15:39:06 -0600

    Add: starting test case for draw, including reference to method #set_draw_test which will be used to initialize a full board for testing

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -86,5 +86,11 @@ RSpec.describe Board do
       
       expect(@board.place_piece(:B, "X")).to eq("invalid column, column full")
     end
+
+    it "can detect when a draw happens due to all columns being full" do
+      @board.set_draw_test
+
+      expect(@board.draw?).to eq(true)
+    end
   end
 end
