8411b511da37158208efd671921bd90d480bbfc0
Joop Stark <stark.joop@gmail.com>
2023-07-21 15:07:07 -0600

    test:place a piece

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,8 +4,14 @@ describe Player do
   describe "set-up" do
     it "t1 exits and has type" do
       expect(dave).to be_a(Player)
+      expect(dave.type).to eq("x")
     end
 
   describe "actions" do
-    it "can place a piece"
+    it "can place a piece" do
+      dave.place_piece("a")
+      dave.place_piece("D")
+
+      expect(board.read_cell(:A, -1)).to eq("X")
+      expect(board.read_cell(:D, -1)).to eq("X")
     it "piece is on its team"
