9d6bef9c07ec439573ded29c6312f242ee97f983
Joop Stark <stark.joop@gmail.com>
2023-07-21 15:19:36 -0600

    test: different inits

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,8 +4,26 @@ describe Player do
   describe "set-up" do
     it "t1 exits and has type" do
       expect(dave).to be_a(Player)
-      expect(dave.type).to eq("x")
+      expect(dave.type).to eq("X")
     end
+
+    it "can take a variety of characters for teams" do
+      andy = Player.new(2)
+      blake = Player.new("x")
+      clyde = Player.new("X")
+      doug = Player.new(1)
+      evie = Player.new("o")
+      flannery = Player.new("O")
+      greg= Player.new(0)
+
+      expect(andy.type).to eq("X")
+      expect(blake.type).to eq("X")
+      expect(clyde.type).to eq("X")
+      expect(doug.type).to eq("O")
+      expect(evie.type).to eq("O")
+      expect(flanner.type).to eq("O")
+      expect(greg.type).to eq("O")
+
 
   describe "actions" do
     it "can place a piece and on his team" do
