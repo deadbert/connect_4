6db8595e68cb093379297ad03476a58c8ed4c31d
Joop Stark <stark.joop@gmail.com>
2023-07-21 14:55:04 -0600

    Players class docs created

---
+++ spec/player_spec.rb
@@ -0,0 +1,10 @@
+require_relative 'spec_helper'
+describe Player do 
+  describe "set-up" do
+    let (:dave) {dave = Player.new("x")}
+    
+    it "t1 exits and has type" do
+
+  describe "actions" 
+    it "can place a piece"
+    it "piece is on its team"
