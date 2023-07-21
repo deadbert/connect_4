4f03f4018c578bdb1045d5a420c399671b8242c0
Joop Stark <stark.joop@gmail.com>
2023-07-21 15:01:22 -0600

    test: player exists and has attribures

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,10 +1,11 @@
 require_relative 'spec_helper'
 describe Player do 
+  let (:dave) {dave = Player.new("x")}
   describe "set-up" do
-    let (:dave) {dave = Player.new("x")}
-    
     it "t1 exits and has type" do
+      expect(dave).to be_a(Player)
+    end
 
-  describe "actions" 
+  describe "actions" do
     it "can place a piece"
     it "piece is on its team"
