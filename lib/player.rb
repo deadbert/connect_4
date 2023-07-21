233bc729df70bf3511c602e6e0aa7d72c580a16d
Joop Stark <stark.joop@gmail.com>
2023-07-21 15:13:37 -0600

    feat: initialize

--- lib/player.rb
+++ lib/player.rb
@@ -1,2 +1,14 @@
 class Player
-  
+  atter_reader:
+  def initialize(type)
+    @type = find_type(type)
+  end
+
+  def find_type(type)
+    case type
+    when 1 || "x" || "X"
+      "X"
+    when 2 || "o" || "O" || 0
+      "O"
+    end
+  end
