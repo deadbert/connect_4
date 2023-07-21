1a85c128b0c39b69d52f0e92cd7143c8e732a2f7
Joop Stark <stark.joop@gmail.com>
2023-07-21 16:45:43 -0600

    create: computer_random and its spec

---
+++ spec/computer_random_spec.rb
@@ -0,0 +1,10 @@
+require_relative "spec_helper"
+describe ComputerRandom do
+  describe "setup" do
+    let (:andrew) {andrew = Player.new("x")}
+    let (:board) {board = Board.new}
+    let (:) {}
+    it "exists and takes left-over type" do
+
+
+    it "can place a piece at a random spot"
