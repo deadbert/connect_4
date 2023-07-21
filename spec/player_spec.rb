2843e6f969169131f95e911d3bce83d00393766a
Joop Stark <stark.joop@gmail.com>
2023-07-21 15:43:38 -0600

    test: corrected varible name for place piece

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,6 +1,7 @@
 require_relative 'spec_helper'
 describe Player do 
   let (:dave) {dave = Player.new("x")}
+  let (:board) {board = Board.new}
   describe "set-up" do
     it "t1 exits and has type" do
       expect(dave).to be_a(Player)
@@ -21,17 +22,20 @@ describe Player do
       expect(clyde.type).to eq("X")
       expect(doug.type).to eq("O")
       expect(evie.type).to eq("O")
-      expect(flanner.type).to eq("O")
+      expect(flannery.type).to eq("O")
       expect(greg.type).to eq("O")
+    end
+  end
 
 
   describe "actions" do
     it "can place a piece and on his team" do
-      dave.place_piece("a")
-      dave.place_piece("D")
+      dave.place_piece(:A, board)
+      dave.place_piece(:D, board)
 
       expect(board.read_cell(:A, -1)).to eq("X")
       expect(board.read_cell(:D, -1)).to eq("X")
     end
   end
+end
 
