c1ef70f2c298372b16035819bd40a18ce35d245d
Joop Stark <stark.joop@gmail.com>
2023-07-21 21:46:49 -0600

    test: computer_random places pieces randomly

--- spec/computer_random_spec.rb
+++ spec/computer_random_spec.rb
@@ -14,23 +14,23 @@ describe ComputerRandom do
     end
   end
 
-  # describe "place piece" do
-  #   it "can place a piece at a random spot" do
-  #     board.setup_game
-  #     andrew = Player.new("x")
-  #     hal.place_piece(board)
-  #     hal.place_piece(board)
-  #     hal.place_piece(board)
-  #     board.render_board
+  describe "place piece" do
+    it "can place a piece at a random spot" do
+      board.setup_game
+      andrew = Player.new("x")
+      hal.place_piece(board)
+      hal.place_piece(board)
+      hal.place_piece(board)
+      board.render_board
     
 
-  #     expect(board.play_area[:A].count("O")).to (be < 3)
-  #     expect(board.play_area[:B].count("O")).to (be < 3)
-  #     expect(board.play_area[:C].count("O")).to (be < 3)
-  #     expect(board.play_area[:D].count("O")).to (be < 3)
-  #     expect(board.play_area[:E].count("O")).to (be < 3)
-  #     expect(board.play_area[:F].count("O")).to (be < 3)
-  #     expect(board.play_area[:G].count("O")).to (be < 3)
-  #   end
+      expect(board.play_area[:A].count("O")).to (be < 3)
+      expect(board.play_area[:B].count("O")).to (be < 3)
+      expect(board.play_area[:C].count("O")).to (be < 3)
+      expect(board.play_area[:D].count("O")).to (be < 3)
+      expect(board.play_area[:E].count("O")).to (be < 3)
+      expect(board.play_area[:F].count("O")).to (be < 3)
+      expect(board.play_area[:G].count("O")).to (be < 3)
+    end
   end
 end
