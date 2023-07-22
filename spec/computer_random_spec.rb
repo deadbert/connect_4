29fd885f2139f6fb2a3dc5fcd78c49b6b78ac4b9
Joop Stark <stark.joop@gmail.com>
2023-07-22 15:02:43 -0600

    bug test: pervented 0 tokens from passing

--- spec/computer_random_spec.rb
+++ spec/computer_random_spec.rb
@@ -1,9 +1,9 @@
 require_relative "spec_helper"
 describe ComputerRandom do
+  let (:board) {board = Board.new}
+  let (:hal) {hal = ComputerRandom.new}
   describe "setup" do
     # let (:andrew) {andrew = Player.new("x")}
-    let (:board) {board = Board.new}
-    let (:hal) {hal = ComputerRandom.new}
     it "exists and takes left-over type" do
       andrew = Player.new("x")
 
@@ -15,6 +15,19 @@ describe ComputerRandom do
   end
 
   describe "place piece" do
+    it "can place a pieces" do
+      board.setup_game
+      andrew = Player.new("x")
+      hal.place_piece(board)
+      hal.place_piece(board)
+      hal.place_piece(board)
+      hal.place_piece(board)
+      hal.place_piece(board)
+      board.render_board
+    
+      expect(board.play_area.values.flatten.count { |token| token.type == "O"}).to eq(5)
+    end
+
     it "can place a piece at a random spot" do
       board.setup_game
       andrew = Player.new("x")
@@ -25,15 +38,15 @@ describe ComputerRandom do
       hal.place_piece(board)
       board.render_board
     
-      expect(board.play_area.values.flatten.count("O")).to eq(5)
+      expect(board.play_area.values.flatten.count { |token| token.type == "O"}).to eq(5)
 
-      # expect(board.play_area[:A].count("O")).to (be < 5)
-      # expect(board.play_area[:B].count("O")).to (be < 5)
-      # expect(board.play_area[:C].count("O")).to (be < 5)
-      # expect(board.play_area[:D].count("O")).to (be < 5)
-      # expect(board.play_area[:E].count("O")).to (be < 5)
-      # expect(board.play_area[:F].count("O")).to (be < 5)
-      # expect(board.play_area[:G].count("O")).to (be < 5)
+      expect(board.play_area[:A].count{ |object| object.type == "O" }).to (be < 5)
+      expect(board.play_area[:B].count{ |object| object.type == "O" }).to (be < 5)
+      expect(board.play_area[:C].count{ |object| object.type == "O" }).to (be < 5)
+      expect(board.play_area[:D].count{ |object| object.type == "O" }).to (be < 5)
+      expect(board.play_area[:E].count{ |object| object.type == "O" }).to (be < 5)
+      expect(board.play_area[:F].count{ |object| object.type == "O" }).to (be < 5)
+      expect(board.play_area[:G].count{ |object| object.type == "O" }).to (be < 5)
     end
   end
 end
