bc751dc71a0e79920d3e98b41f830a2f31abc7e7
Nick Spencer <strangebrew202@gmail.com>
2023-07-22 09:09:52 -0600

    Add: user prompt now checks for quit condition, draw check now prints a custom message before exiting loop

--- lib/board_test_runner.rb
+++ lib/board_test_runner.rb
@@ -18,6 +18,8 @@ if choice.downcase == "p"
   puts "would you like X's or O's?('x/o')"
   selection = gets.chomp
   player_1 = Player.new(selection)
+else
+  on = false
 end
 
 game = TurnManager.new(board, player_1, cpu)
@@ -26,26 +28,9 @@ while on
   # test for win conditions
   game.cpu_turn
   # test for win conditions
-  break if board.draw?
+  if board.draw?
+    puts "It's a draw"
+    board.render_board
+    break
+  end
 end
-board.render_board
-puts "Game over"
-
-
-
-# while on
-#   board.render_board
-#   puts "Select a column for your piece(A-G)"
-#   column = gets.chomp.upcase.intern
-#   new_token = player_1.place_piece(column, board)
-#   if new_token == "invalid column, column full" || new_token == "invalid column"
-#     until new_token != "invalid column, column full" && new_token != "invalid column"
-#       puts "#{new_token}"
-#       puts "Select another row(A-G)"
-#       column = gets.chomp.upcase.intern
-#       new_token = player_1.place_piece(column, board)
-#     end
-#   end
-#   # check win conditions
-#   # on = false if board.draw?
-# end
