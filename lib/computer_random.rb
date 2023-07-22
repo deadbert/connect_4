6556474c11c2c04f4741bcefbb822b2a4e43578f
Joop Stark <stark.joop@gmail.com>
2023-07-21 22:03:58 -0600

    feat:ComputerRandom can initialize

---
+++ lib/computer_random.rb
@@ -0,0 +1,16 @@
+class ComputerRandom
+  attr_reader :type, :enemy
+  def initialize(type = "O")
+    @type = type
+    @enemy = OPPONENT[type]
+  end
+
+  # OPPONENT = {"O"=>"X", "X"=>"O"}
+  # COLUMNS = ["A", "B", "C", "D", "E", "F", "G"]
+
+  # def place_piece(board)
+  #   board.place_piece(COLUMNS[rand(0..6)], @type)
+  # end
+end
+
+
