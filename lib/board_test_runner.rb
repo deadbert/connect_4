35446fb87f54636bdcda929c229045a78ef80326
Nick Spencer <strangebrew202@gmail.com>
2023-07-22 18:38:07 -0600

    Add: win_checker with #check_vertical_win and #check_horizontal_win to test win conditions in runner

--- lib/board_test_runner.rb
+++ lib/board_test_runner.rb
@@ -3,6 +3,7 @@ require_relative 'token'
 require_relative 'player'
 require_relative 'turn_manager'
 require_relative 'computer_random'
+require_relative 'win_checker'
 
 
 board = Board.new
@@ -14,8 +15,11 @@ def main_menu(board, cpu)
   choice = gets.chomp
 
   if choice.downcase == "p"
-    puts "would you like X's or O's?('x/o')"
-    selection = gets.chomp
+    selection = ''
+    until selection == "x" || selection == "o"
+      puts "would you like X's or O's?('x/o')"
+      selection = gets.chomp
+    end
     player_1 = Player.new(selection)
     board.setup_game
     game = TurnManager.new(board, player_1, cpu)
@@ -27,11 +31,22 @@ end
 
 
 def game_loop(board, game, cpu)
+  checker = WinChecker.new
   loop do
     game.start_turn
-    # test for win conditions
+    if checker.check_vertical_win(board) || checker.check_horizontal_win(board)
+      board.render_board
+      board.clear_board
+      puts "Game Over"
+      break
+    end
     game.cpu_turn
-    # test for win conditions
+    if checker.check_vertical_win(board) || checker.check_horizontal_win(board)
+      board.render_board
+      board.clear_board
+      puts "Game Over"
+      break
+    end
     if board.draw?
       puts "It's a draw"
       board.render_board
