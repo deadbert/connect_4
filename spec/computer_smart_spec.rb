f3bba38b874f2e61abfebaccecd1e8eb450fbd9d
Joop Stark <stark.joop@gmail.com>
2023-07-24 15:55:08 -0600

    test:diagonal test

--- spec/computer_smart_spec.rb
+++ spec/computer_smart_spec.rb
@@ -8,7 +8,7 @@ let (:board) {board = Board.new}
 let (:checker) {checker = CheckerCriteria.new}
 let (:hal) {hal = ComputerSmart.new}
 
-it "can give positions of all pieces of one type" do
+it "find empty" do
   board.setup_game
 
   dave.place_piece(:D, board)
@@ -25,6 +25,32 @@ it "can give positions of all pieces of one type" do
 
   board.render_board
 
-  expect(hal.find_empty(:G, board)).to eq("O")
+  expect(hal.find_empty(6, board)).to eq(4)
   end
+  
+  it "test diagonal" do
+    board.setup_game
+
+    dave.place_piece(:B, board)
+    dave.place_piece(:C, board)
+    dave.place_piece(:C, board)
+    dave.place_piece(:D, board)
+    dave.place_piece(:D, board)
+    dave.place_piece(:D, board)
+    sally.place_piece(:A, board)
+    sally.place_piece(:B, board)
+    sally.place_piece(:C, board)
+  
+    board.render_board
+  
+    hal.tester(3, board, 1 , -1)
+
+    expect(hal.main_counter).to eq(3)
+  end
+
+
+
+
+
+
 end
