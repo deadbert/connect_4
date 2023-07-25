1357a653b4ce3bde349edd8bced3fddfd6c7f785
Nick Spencer <strangebrew202@gmail.com>
2023-07-25 13:40:21 -0600

    Add: start screen with p or q option, comments to increase readability, changed methods in :mouse_down to only make method calls through TurnManager game object

--- connect_4_graphics.rb
+++ connect_4_graphics.rb
@@ -8,11 +8,11 @@ player_1 = Player.new("X")
 cpu = ComputerRandom.new(player_1.type)
 game = TurnManager.new(board, player_1, cpu)
 
-set title: "Connect 4"
-set background: "white"
 set width: 775
 set height: 800
 
+# cordinate data for rendering blank squares, as well as X's and O's when placed
+# symbols correspond to the return from Player.place_piece and TurnManager.cpu_turn
 cordinates =  {
   A5: [50, 700], A4: [50, 600], A3: [50, 500], A2: [50, 400], A1: [50, 300], A0: [50, 200],
   B5: [150, 700], B4: [150, 600], B3: [150, 500], B2: [150, 400], B1: [150, 300], B0: [150, 200],
@@ -24,15 +24,21 @@ cordinates =  {
 
 }
 
-cordinates.each do |key, values|
-  Square.new(
-    x: values[0], y: values[1],
-    size: 50,
-    color: 'blue', opacity: 0.2,
-    z: 0
-  )
+# Uses cordinates Hash to render blank square positions representing connect 4 board
+def render_game(cordinates, board)
+  set title: "Connect 4"
+  set background: "white"
+  cordinates.each do |key, values|
+    Square.new(
+      x: values[0], y: values[1],
+      size: 50,
+      color: 'blue', opacity: 0.2,
+      z: 0
+    )
+  end
 end
 
+# Helper method returns the corresponding symbol for selected column to drop token
 def find_column(event)
   return :A if event.x > 50 && event.x < 100
   return :B if event.x > 150 && event.x < 200
@@ -41,8 +47,11 @@ def find_column(event)
   return :E if event.x > 450 && event.x < 500
   return :F if event.x > 550 && event.x < 600
   return :G if event.x > 650 && event.x < 700
+  return "out of range"
 end
 
+# method gathers cordinates token is placed at, and uses cordinates hash to render
+# token in the correct placement
 def render_token(cords, type, cordinates)
   Text.new(
     "#{type}",
@@ -53,13 +62,55 @@ def render_token(cords, type, cordinates)
   )
 end
 
+# Images used for showing a game over message on screen
 def render_game_over
-  set background: 'blue'
   Text.new(
     "Game Over",
-    x: 125, y: 325,
+    x: 125, y: 0,
     style: 'bold',
     size: 100,
+    color: 'red'
+  )
+  Text.new(
+    "press p to play again, or q to quit",
+    x: 130, y: 125,
+    size: 35,
+    color: 'red'
+  )
+end
+
+# key down and mouse down methods for game selection and token position selection
+on :key_down do |event|
+  if event.key == "p"
+    clear
+    render_game(cordinates, board)
+  end
+  if event.key == "q"
+    close
+  end
+end
+
+on :mouse_down do |event|
+    column = find_column(event)
+    render_token(game.player.place_piece(column, board), game.player.type, cordinates)
+    cpu_token = game.cpu_turn
+    render_token(cpu_token, game.cpu.type, cordinates)
+    board.render_board
+    if checker.check_win_conditions(board)
+      board.clear_board
+      board.setup_game
+      render_game_over
+    end
+end
+# key down and mouse down methods for game selection and token position selection
+
+# set up the first start screen with p to play and q to quit options
+def render_start_screen
+  Text.new(
+    "Welcome to connect 4",
+    x: 125, y: 325,
+    style: 'bold',
+    size: 50,
     color: 'red'
   )
   Text.new(
@@ -70,27 +121,8 @@ def render_game_over
   )
 end
 
-on :key_down do |event|
-  if event.key == "p"
-    puts "it's p"
-  end
-  if event.key == "q"
-    puts "it's a q"
-  end
-end
-
-on :mouse_down do |event|
-    column = find_column(event)
-    render_token(player_1.place_piece(column, board), player_1.type, cordinates)
-    cpu_token = game.cpu_turn
-    render_token(cpu_token, cpu.type, cordinates)
-    board.render_board
-    if checker.check_win_conditions(board)
-      board.clear_board
-      clear 
-      render_game_over
-    end
-end
+render_start_screen
+# set up the first start screen with p to play and q to quit options
 
 show
 
